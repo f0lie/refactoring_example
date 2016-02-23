5cb6dd162226789ca78cd7d057b0ef47c64f0af2
Alex Diep <alexdiep2501@gmail.com>
2016-02-23 18:35:05 -0500

    added display class

---
+++ display.py
@@ -0,0 +1,29 @@
+from blessed import Terminal
+from time import sleep
+
+term = Terminal()
+
+class Display():
+    hide_cursor = term.hidden_cursor
+    height = term.height
+    width = term.width
+
+    def __init__(self, point_list=None, delay=0.05):
+        if point_list is None:
+            self.points = []
+        self.points = point_list
+        self.delay = delay
+        print(term.clear)
+
+    def draw(self):
+        for point in self.points:
+            print(term.move(point.y_pos, point.x_pos) + '*')
+        sleep(self.delay)
+        print(term.clear)
+
+    def __enter__(self):
+        print(term.hide_cursor)
+        return this
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(term.normal_cursor)
--- main_3.py
+++ main_3.py
@@ -1,3 +1,6 @@
+from display import Display
+from collections import namedtuple
+
 class Point():
     def __init__(self, x_pos=0, y_pos=0, x_velo=0, y_velo=0):
         self.x_pos = x_pos
@@ -11,43 +14,31 @@ class Point():
         self.y_pos += self.y_velo
 
     def bounce(self, grid_size):
-        if self.x_pos < 0 or self.x_pos > grid_size - 1:
+        if self.x_pos < 0 or self.x_pos > grid_size.width - 1:
             self.x_velo *= -1
             self.x_pos += self.x_velo
 
-        if self.y_pos < 0 or self.y_pos > grid_size - 1:
+        if self.y_pos < 0 or self.y_pos > grid_size.height - 1:
             self.y_velo *= -1
             self.y_pos += self.y_velo
 
-class Grid():
-    def __init__(self, point_list=None, grid_size=5):
-        self.grid = [[0]*grid_size for i in range(grid_size)]
-        self.grid_size = grid_size
+Grid_Size = namedtuple('Grid_Size', 'height width')
 
-        if point_list is None:
-            point_list = []
-        self.points = point_list
+def main():
+    points = [Point(0, 0, 1, 1),
+              Point(0, 0, 0, 1),
+              Point(4, 4, -1, -1),
+              Point(4, 4, 0, -1)]
+    display = Display(points, 0.05)
 
-    def step(self):
-        for point in self.points:
-            self.grid[point.y_pos][point.x_pos] = 1
+    grid_size = Grid_Size(display.height - 5, display.width)
 
-        for row in self.grid:
-            print(row)
-        print()
-
-        for point in self.points:
-            self.grid[point.y_pos][point.x_pos] = 0
-
-        for point in self.points:
+    for i in range(100):
+        display.draw()
+        for point in points:
             point.move()
-            point.bounce(self.grid_size)
+            point.bounce(grid_size)
 
-GRID = Grid([Point(0, 0, 1, 1),
-             Point(0, 0, 0, 1),
-             Point(4, 4, -1, -1),
-             Point(4, 4, 0, -1)],
-            grid_size=5)
-
-for i in range(5):
-    GRID.step()
+if __name__ == '__main__':
+    with Display.hide_cursor():
+        main()
